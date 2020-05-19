09b89fcba658e2dc2ac23b3c4b08dade5b5366b5
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-05-20 00:12:06 +0900

    초기 커밋.

---
+++ myrandomrealnumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myrandomrealnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int a=random.Next(0, 1000);
+            int b=0;
+            
+            do{
+                Console.Write("숫자를 입력해주세요:");
+                b=int.Parse(Console.ReadLine());
+
+                if (a > b)
+                    Console.WriteLine(b + "보다는 큰 숫자입니다.\n");
+                else if (a < b)
+                    Console.WriteLine(b + "보다는 작은 숫자입니다.\n");
+                else
+                    Console.WriteLine("정답입니다!\n");
+            } while (a != b) ;
+        }
+    }
+}
